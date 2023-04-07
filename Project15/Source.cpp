4313afa29ee6de0a9ff04fe6aed9abebdb1a21fa
ShElFyE <daniil.komarchuk0@gmail.com>
2023-04-08 01:09:33 +0300

    Добавил считывание - сколько будет отрабатывать цикл

--- Project15/Source.cpp
+++ Project15/Source.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-	return 0;
+    clock_t start = clock();
+    for (int i = 0; i < 10000000000000000000; i++) {
+    }
+    clock_t end = clock();
+    double duration = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Programa otrabotala za " << duration << " secund\n";
+    return 0;
 }
