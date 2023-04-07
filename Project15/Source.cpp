0cc44e7cb755cd2a9727ef071687437de9c5d1d0
ShElFyE <128957553+ShElFyE@users.noreply.github.com>
2023-04-08 02:15:54 +0300

    Update Source.cpp

--- Project15/Source.cpp
+++ Project15/Source.cpp
@@ -1,29 +1,11 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-
 int main() {
-    const int n = 10000000;
-    double x, y;
-    double res = 0.0;
     clock_t start = clock();
-    for (int i = 0; i < 100000000; i++) {
-    }
-    clock_t mid = clock();
-    for (int i = 0; i < n; i++) {
-        x = sin(i);
-        y = cos(i);
-        res += x * y;
+    for (int i = 0; i < 1000000; i++) {
     }
     clock_t end = clock();
-    double duration1 = double(mid - start) / CLOCKS_PER_SEC;
-    double duration2 = double(end - mid) / CLOCKS_PER_SEC;
-    cout << "Mathematicheskie deistviya programa otrabotala za : " << duration2 << " secund\n";
-    if (duration2 > 0.0) {
-        cout << "Programa otrabotala za " << duration1 + duration2 << " secund\n";
-    }
-    else {
-        cout << "Programa otrabotala za " << duration1 << " secund\n";
-    }
+    double duration = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Programa otrabotala za " << duration << " secund\n";
     return 0;
 }
